1c0cd42bb04d2fa971c7143c4ec6e3aa1c847fa3
Aya Nitta <aya@AyanoMacBook.local>
2020-01-23 20:51:36 +0900

    タブのタイトルを変更しました！

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -45,9 +45,9 @@ export async function unstable_getStaticProps() {
 export default ({ posts = [] }) => {
   return (
     <>
-      <Header titlePre="Blog" />
+      <Header titlePre="ねぽたや" />
       <div className={`${sharedStyles.layout} ${blogStyles.blogIndex}`}>
-        <h1>My Notion Blog</h1>
+        <h1>Blog</h1>
         {posts.length === 0 && (
           <p className={blogStyles.noPosts}>There are no posts yet</p>
         )}
