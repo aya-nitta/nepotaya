b4b21cd40f58133bf1f97f27376ef427336ba173
Aya Nitta <aya@AyanoMacBook.local>
2020-01-28 14:43:16 +0900

    homeをねぽたやに変更しました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
+  { label: 'ねぽたとは', page: '/' },
   { label: 'Blog', page: '/blog' },
   { label: 'Contact', page: '/contact' },
   { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
@@ -19,7 +19,7 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} ` : ''} </title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
