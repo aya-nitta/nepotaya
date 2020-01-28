2108867476517b38a7efc64bd79ed31e8cecc45f
Aya Nitta <aya@AyanoMacBook.local>
2020-01-28 14:44:14 +0900

    Deploy your ownを削除しました

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,15 @@
+import * as React from 'react'
 import '../styles/global.css'
 import ExtLink from '../components/ext-link'
 
-export default ({ Component, pageProps }) => (
+const component: React.FC = ({ Component, pageProps }) => (
   <>
     <Component {...pageProps} />
 
     <footer>
-      <span>Deploy your own!</span>
-      <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master">
-        <img
-          src="https://zeit.co/button"
-          height={46}
-          width={132}
-          alt="deploy to ZEIT button"
-        />
-      </ExtLink>
-      <span>
-        or{' '}
-        <ExtLink href="https://github.com/ijjk/notion-blog">
-          view source
-        </ExtLink>
-      </span>
+      <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master" />
     </footer>
   </>
 )
+
+export default component
