01b63619eb19d0d92773c6f502021ef247c1e602
Aya Nitta <aya@AyanoMacBook.local>
2020-01-29 21:50:52 +0900

    ねぽたとはの説明を変更しました

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 // ねぽたとは
 import * as React from 'react'
 import Header from '~/components/header'
-import ExtLink from '../components/ext-link'
-import Features from '../components/features'
+// import ExtLink from '../components/ext-link'
+// import Features from '../components/features'
 import sharedStyles from '../styles/shared.module.css'
 
 const Index: React.FC = () => (
@@ -11,38 +11,11 @@ const Index: React.FC = () => (
     <div className={sharedStyles.layout}>
       <img src="/icon.png" alt="icon" width="100" height="100" />
       <h1>ハジメマシテ。</h1>
-      <h2>まずは自己紹介から参りましょうか…</h2>
+      <h2>まずは自己紹介から参りましょうか…ｵﾌｫﾌｫﾌｫ…</h2>
 
       <div className="explanation">
         <p>
-          This is a statically generated{' '}
-          <ExtLink href="https://nextjs.org">Next.js</ExtLink> site with a{' '}
-          <ExtLink href="https://notion.so">Notion</ExtLink> powered blog that
-          is deployed with <ExtLink href="https://zeit.co">ZEIT</ExtLink>. It
-          leverages some upcoming features in Next.js like{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/9524">
-            SSG support
-          </ExtLink>{' '}
-          and{' '}
-          <ExtLink href="https://github.com/zeit/next.js/issues/8626">
-            built-in CSS support
-          </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          pages id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/ijjk/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new pages id. To get your token from Notion, login and
-          look for a cookie under www.notion.so with the name `token_v2`. After
-          finding your token and your blogs page id you should be good to go!
+          性別は女、旦那さんとノルウェージャンフォレストキャットのプータローと暮らしています。
         </p>
       </div>
     </div>
