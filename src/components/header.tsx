49118bf9b8950a8b6d3ddc5e7ed250c29c7c2e3a
Aya Nitta <aya@AyanoMacBook.local>
2020-02-05 22:15:21 +0900

    headerにねぽたやを追加しました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -24,6 +24,7 @@ export default ({ titlePre = '' }) => {
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
+        <h1>ねぽたや</h1>
         <meta name="og:title" content="My Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
         <meta name="twitter:site" content="@_ijjk" />
