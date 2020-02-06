551181f9bf67f43d0bc3ce8564c2b2c2377fbc70
soichiro-nitta <soic1226@gmail.com>
2020-02-06 19:02:15 +0900

    Clean: _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,6 @@ import ExtLink from '../components/ext-link'
 const Component = ({ Component, pageProps }): React.ReactElement => (
   <>
     <Component {...pageProps} />
-
     <footer>
       <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master" />
     </footer>
