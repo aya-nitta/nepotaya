5262dbf5bd2788c52172a6d7e06dbbd8333c28d4
Aya Nitta <aya@AyanoMacBook.local>
2020-02-10 13:30:16 +0900

    WIP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import '../styles/global.css'
+// import '../styles/global.css'
 import ExtLink from '../components/ext-link'
 
 const Component = ({ Component, pageProps }): React.ReactElement => (
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,8 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import Header from '~/components/header'
-import sharedStyles from '../styles/shared.module.css'
+// import sharedStyles from '../styles/shared.module.css'
+
 // (2) Types層
 // type ContainerProps = {}
 // type Props = { } & ContainerProps
@@ -11,38 +12,57 @@ import sharedStyles from '../styles/shared.module.css'
 const Component: React.FC = () => (
   <>
     <Header titlePre="ねぽたとは" />
-    <div className={sharedStyles.layout}>
-      <a href="/name">
-        <img src="/icon.png" alt="icon" width="240" height="240" />
-      </a>
-      <h1>ハジメマシテ。</h1>
-      <h2>まずは自己紹介から参りましょうか…</h2>
 
-      <p>
-        性別は女
-        <br />
-        旦那さんとノルウェージャンフォレストキャットのプータローと暮らしています。
-        <br />
-        よくツイッターにおりますので、気軽にリプください。
-        <br />
-        PUBG MOBILEのID （6106283530）{' '}
-      </p>
+    <a href="/name">
+      <img src="/icon.png" alt="icon" width="240" height="240" />
+    </a>
+    <h1>ハジメマシテ。</h1>
+    <h2>まずは自己紹介から参りましょうか…</h2>
 
-      <h1>Skillset</h1>
-      <p>html,css</p>
-    </div>
+    <p>
+      性別は女
+      <br />
+      旦那さんとノルウェージャンフォレストキャットのプータローと暮らしています。
+      <br />
+      よくツイッターにおりますので、気軽にリプください。
+      <br />
+      PUBG MOBILEのID （6106283530）{' '}
+    </p>
+
+    <h1>Skillset</h1>
+    <p>html,css</p>
   </>
 )
 // (4) Style層
 const StyledComponent = styled(Component)`
-  color: blue;
-  .btn {
-    color: yellow;
+  a {
   }
-  > button {
-    color: blue;
+
+  h1 {
+    margin: 5px;
+    font-size: 50px;
+    font-weight: 300;
+    text-align: center;
+    color: #888;
+  }
+
+  h2 {
+    margin: 5px;
+    font-size: 50px;
+    font-weight: 300;
+    text-align: center;
+    color: #888;
+  }
+
+  p {
   }
 `
+// color: blue;
+// .btn {
+//   color: yellow;
+// }
+// > button {
+
 // (5) Container層
 const Container: React.FC = props => {
   return <StyledComponent {...props} />
