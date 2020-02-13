1cc14c0502a880e151abebb253aa8d3586ad5898
soichiro-nitta <soic1226@gmail.com>
2020-02-13 17:01:38 +0900

    refactor(src/pages/contact.tsx): 解説した

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,42 +1,42 @@
-// contact
-// (1) import層
 import * as React from 'react'
 import styled from 'styled-components'
 import Header from '../components/header'
 import ExtLink from '../components/ext-link'
-
 import contactStyles from '../styles/contact.module.css'
-
 import Instagram from '../components/svgs/instagram'
 import Twitter from '../components/svgs/twitter'
-// import Envelope from '../components/svgs/envelope'
+import Envelope from '../components/svgs/envelope'
 
-// (2) Types層
 type ContainerProps = {}
-type Props = { className: string } & ContainerProps
+type Props = {
+  className: string
+  contacts: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Icon: React.FC
+    alt: string
+    link: string
+  }[]
+} & ContainerProps
 
-// (3) DOM層
-const Component: React.FC<Props> = props => (
-  <div className={props.className}>
-    <Header titlePre="Contact" />
-
-    <img src="/icon.png" alt="icon" width="240" height="240" />
-
-    <h1>Contact</h1>
-
-    <div className={contactStyles.links}>
-      {contacts.map(({ Comp, link, alt }) => {
-        return (
-          <ExtLink key={link} href={link} aria-label={alt}>
-            <Comp height={32} />
-          </ExtLink>
-        )
-      })}
+const Component: React.FC<Props> = props => {
+  return (
+    <div className={props.className}>
+      <Header titlePre="Contact" />
+      <img src="/icon.png" alt="icon" width="240" height="240" />
+      <h1>Contact</h1>
+      <div className={contactStyles.links}>
+        {props.contacts.map(({ Icon, link, alt }) => {
+          return (
+            <ExtLink key={link} href={link} aria-label={alt}>
+              <Icon />
+            </ExtLink>
+          )
+        })}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
-// (4) Style層
 const StyledComponent = styled(Component)`
   color: #888;
   text-align: center;
@@ -57,34 +57,26 @@ const StyledComponent = styled(Component)`
     font-size: 30px;
   }
 `
-// color: blue;
-// .btn {
-//   color: yellow;
-// }
-// > button {
 
-// (5) Container層
 const Container: React.FC = props => {
-  return <StyledComponent className="contact" {...props} />
+  const contacts = [
+    {
+      Icon: Twitter,
+      alt: 'twitter icon',
+      link: 'https://twitter.com/_nenenemo'
+    },
+    {
+      Icon: Instagram,
+      alt: 'instagram icon',
+      link: 'https://www.instagram.com/_nenenemu'
+    },
+    {
+      Icon: Envelope,
+      alt: 'envelope icon',
+      link: 'mailto:jj@jjsweb.site?subject=Notion Blog'
+    }
+  ]
+  return <StyledComponent className="contact" contacts={contacts} {...props} />
 }
-
-const contacts = [
-  {
-    Comp: Twitter,
-    alt: 'twitter icon',
-    link: 'https://twitter.com/_nenenemo',
-  },
-  {
-    Comp: Instagram,
-    alt: 'instagram icon',
-    link: 'https://www.instagram.com/_nenenemu',
-  },
-
-  // {
-  //   Comp: Envelope,
-  //   alt: 'envelope icon',
-  //   link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
-  // },
-]
 
 export default Container
