e33f752808e240b3546decb88e6016f7842b0867
Aya Nitta <aya@AyanoMacBook.local>
2020-02-13 16:10:25 +0900

    refactor(src/components/header.tsx): ５層で構成するスタイルに変更しました

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,18 @@
+// (1) import層
 import Link from 'next/link'
 import Head from 'next/head'
 import ExtLink from './ext-link'
 import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
+import styled from 'styled-components'
+
+// (2) Types層
+// type ContainerProps = {}
+// type Props = { className: string } & ContainerProps
+
+// (3) DOM層
+// const Component: React.FC<Props> = props => (
+//   <div className={props.className}></div>
 
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'ねぽたとは', page: '/' },
@@ -49,3 +59,37 @@ export default ({ titlePre = '' }) => {
     </header>
   )
 }
+
+// // (4) Style層
+// const StyledComponent = styled(Component)`
+//   color: #888;
+//   text-align: center;
+//   a {
+//     margin: 5px;
+//     font-size: 30px;
+//   }
+//   h1 {
+//     margin: 5px;
+//     font-size: 30px;
+//   }
+//   h2 {
+//     margin: 5px;
+//     font-size: 30px;
+//   }
+//   p {
+//     margin: 5px;
+//     font-size: 30px;
+//   }
+// `
+// color: blue;
+// .btn {
+//   color: yellow;
+// }
+// > button {
+
+// (5) Container層
+// const Container: React.FC = props => {
+//   return <StyledComponent className="header" {...props} />
+// }
+
+// export default Container
