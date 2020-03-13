667746d9f7d15c8227dde0f35fd4a3df886fbe3c
Aya Nitta <aya@AyanoMacBook.local>
2020-03-13 17:37:55 +0900

    refactor(src/pages): 内容を修正しました

--- src/pages/puuuu.tsx
+++ src/pages/puuuu.tsx
@@ -9,17 +9,15 @@ type Props = { className: string } & ContainerProps
 // (3) DOM層
 const Component: React.FC<Props> = props => (
   <div className={props.className}>
-    <img src="/puuuu.png" alt="icon" width="240" height="240" />
-    <h3>一緒に暮らしているノルウェーフォレストキャットのプータローです。</h3>
-    <br />
-    通称ぷーち。
     <p>
-      性別は男です。
+      一緒に暮らしているノルウェーフォレストキャットのプータローです。 <br />
+      通称ぷーち。 性別は男です。
       <br />
       生まれは2019年8月15日。
       <br />
       オモチャください。
     </p>
+    <img src="/puuuu.png" alt="icon" width="240" height="240" />
   </div>
 )
 // (4) Style層
