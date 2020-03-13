1ffa03ea108a313466d9f84c7a526f85bd01a9eb
Aya Nitta <aya@AyanoMacBook.local>
2020-03-13 17:36:44 +0900

    refactor(src/components/svg): height,widthを追加しました

--- src/components/svgs/instagram.tsx
+++ src/components/svgs/instagram.tsx
@@ -1,9 +1,9 @@
 export default props => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    height="35px"
+    height="50px"
+    width="50px"
     viewBox="0 0 511 511.9"
-    width="35px"
   >
     <g>
       <path
