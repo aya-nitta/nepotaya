424f044ec7cd1591dd82fdb97c66d8b94e9eebeb
Aya Nitta <aya@AyanoMacBook.local>
2020-03-13 17:39:11 +0900

    refactor(src/pages/contact.tsx): twitter,publicを追加しました

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import ExtLink from '../components/ext-link'
 import Instagram from '../components/svgs/instagram'
 import Twitter from '../components/svgs/twitter'
-import Envelope from '../components/svgs/envelope'
+import Email from '../components/svgs/email'
 
 type ContainerProps = {}
 type Props = {
@@ -18,8 +18,6 @@ type Props = {
 const Component: React.FC<Props> = props => {
   return (
     <div className={props.className}>
-      <h3>Contact</h3>
-
       {props.contacts.map(({ Icon, link, alt }) => {
         return (
           <ExtLink key={link} href={link} aria-label={alt}>
@@ -27,21 +25,40 @@ const Component: React.FC<Props> = props => {
           </ExtLink>
         )
       })}
+      <p>こちらからお問い合わせください！</p>
+      <a
+        className="twitter-timeline"
+        data-width="600"
+        data-height="800"
+        href="https://twitter.com/_nenenemo?ref_src=twsrc%5Etfw"
+      >
+        Tweets by _nenenemo
+      </a>{' '}
+      <script async src="https://platform.twitter.com/widgets.js" />
     </div>
   )
 }
 
 const StyledComponent = styled(Component)`
-  background-color: #fff;
   color: black;
   margin: 0 auto;
   width: 600px;
-  h3 {
-    display: block;
+  svg {
+    padding: 15px 75px;
+  }
+  p {
     text-align: center;
+  }
+  a.twitter-timeline {
+    text-decoration: none; /* リンクの下線を消す */
+    color: #888;
+  }
+  path.active-path:hover {
+    color: white;
   }
 `
 
+//src/components/svg
 const Container: React.FC = props => {
   const contacts = [
     {
@@ -55,7 +72,7 @@ const Container: React.FC = props => {
       link: 'https://www.instagram.com/_nenenemu'
     },
     {
-      Icon: Envelope,
+      Icon: Email,
       alt: 'envelope icon',
       link: 'mailto:jj@jjsweb.site?subject=Notion Blog'
     }
