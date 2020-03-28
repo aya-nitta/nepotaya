6ca9ec4661453a908f498cd24d1acf3f82c36569
Aya Nitta <aya@AyanoMacBook.local>
2020-03-28 12:45:01 +0900

    refactor(src/pages/puuuu): cssの内容を修正しました

--- src/pages/puuuu.tsx
+++ src/pages/puuuu.tsx
@@ -17,7 +17,10 @@ const Component: React.FC<Props> = props => (
       <br />
       オモチャください。
     </p>
-    <img src="/puuuu.png" alt="icon" width="240" height="240" />
+    <div className="putaro">
+      <div className="background" />
+      <img src="/puuuu.png" />
+    </div>
   </div>
 )
 // (4) Style層
@@ -25,9 +28,48 @@ const StyledComponent = styled(Component)`
   color: black;
   margin: 0 auto;
   width: 600px;
-  h3 {
-    display: block;
-    text-align: center;
+  > .putaro {
+    /* positionは、relativeで範囲を決めてabsoluteで動かそう！ */
+    position: relative;
+    width: 200px;
+    height: 200px;
+  }
+  > * > .background {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: linear-gradient(
+      -45deg,
+      rgb(255, 166, 73),
+      rgb(247, 100, 91),
+      rgb(128, 94, 212)
+    );
+    /* infiniteは無限ループ */
+    animation: load 2s linear 0s infinite normal none running;
+  }
+  > * > img {
+    /* absoluteは絶対配置を指定する値 */
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    margin: auto;
+    width: calc(100% - 5%);
+    height: calc(100% - 5%);
+    background: rgb(49, 55, 70);
+    border-radius: 50%;
+  }
+
+  @keyframes load {
+    0% {
+      -webkit-transform: rotate(0deg);
+      transform: rotate(0deg);
+    }
+    100% {
+      -webkit-transform: rotate(360deg);
+      transform: rotate(360deg);
+    }
   }
 `
 // (5) Container層
