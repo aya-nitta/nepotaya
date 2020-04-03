dae49ac426f063ca60650865d5a8cda4a4e75613
Aya Nitta <aya@AyanoMacBook.local>
2020-04-03 11:16:21 +0900

    refactor(src/pages/_app.tsx): <Contact />を追加しました

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import ExtLink from '../components/ext-link'
 import Header from '~/components/_app/Header'
+import Contact from '~/components/_app/Contact'
 
 const Component = ({ Component, pageProps }): React.ReactElement => (
   <>
     <Header />
+    <Contact />
     <Component {...pageProps} />
     <footer>
       <ExtLink href="https://zeit.co/new/project?template=https://github.com/ijjk/notion-blog/tree/master" />
